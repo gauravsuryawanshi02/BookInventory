ae98fd61578a9e1741810be54028fbead868f79d
Gaurav Suryawanshi <jsuryaga@jci.com>
2024-04-09 11:19:24 +0530

    Added Bulky inventory

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContex _db;
+        public CategoryController(ApplicationDbContex dbContex)
+        {
+            _db = dbContex;
+        }
+        //GET
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CuntomError", "Fields cannot be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Record created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categorydb = _db.Categories.Find(id);
+
+            if (categorydb == null)
+            {
+                return NotFound();
+            }
+            return View(categorydb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CuntomError", "Fields cannot be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categorydb = _db.Categories.Find(id);
+
+            if (categorydb == null)
+            {
+                return NotFound();
+            }
+            return View(categorydb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id); if (obj == null) { NotFound(); }
+            _db.Categories.Remove(obj);
+             _db.SaveChanges();
+             return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContex.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContex :DbContext
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
